5a937dcb77a9cf5bda8e7a261cbfe2ec16c83cbb
seba1510 <48321015+seba1510@users.noreply.github.com>
2019-12-10 09:02:27 +0100

    Update zadanie05.js

--- js/zadanie05.js
+++ js/zadanie05.js
@@ -1,4 +1,4 @@
 var buttons = document.querySelectorAll("button");
 for(i=0; i<buttons.length; i++){
-	buttons[i].addEventListener('click',function tak(){ document.getElementById("container").innerHTML=this.getAttribute("data-text")});
+	buttons[i].addEventListener('click',function fun(){ document.getElementById("container").innerHTML=this.getAttribute("data-text")});
 }
